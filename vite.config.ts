b57299a3392570617d80c3519534df819bd7658b
d.vorobjev <d.vorobjev@zordoc.uz>
2023-07-06 00:21:34 +0500

    setup eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import path from "path"
+import vue from "@vitejs/plugin-vue"
+import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "~app": path.resolve("src/app"),
+      "~entities": path.resolve("src/entities"),
+      "~features": path.resolve("src/features"),
+      "~pages": path.resolve("src/pages"),
+      "~shared": path.resolve("src/shared"),
+      "~widgets": path.resolve("src/widgets"),
+    },
+  },
 })
