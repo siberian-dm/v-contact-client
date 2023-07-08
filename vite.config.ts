192bba73b71c66286008d62ea0007e6f514e4d7f
d.vorobjev <d.vorobjev@zordoc.uz>
2023-07-09 00:34:11 +0500

    changed configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,30 @@
-import path from "path"
-import vue from "@vitejs/plugin-vue"
-import { defineConfig } from "vite"
+import path from 'path';
+import vue from '@vitejs/plugin-vue';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/vite';
+import { defineConfig } from 'vite';
+import EnvironmentPlugin from 'vite-plugin-environment';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    tsconfigPaths(),
+    EnvironmentPlugin('all', { prefix: 'VITE_' }),
+    Components({
+      resolvers: [AntDesignVueResolver()],
+      dts: 'src/components.d.ts',
+    }),
+  ],
   resolve: {
     alias: {
-      "~app": path.resolve("src/app"),
-      "~entities": path.resolve("src/entities"),
-      "~features": path.resolve("src/features"),
-      "~pages": path.resolve("src/pages"),
-      "~shared": path.resolve("src/shared"),
-      "~widgets": path.resolve("src/widgets"),
+      '~app': path.resolve('src/app'),
+      '~entities': path.resolve('src/entities'),
+      '~features': path.resolve('src/features'),
+      '~pages': path.resolve('src/pages'),
+      '~shared': path.resolve('src/shared'),
+      '~widgets': path.resolve('src/widgets'),
     },
   },
-})
+});
