aac29620de7adc0e9948f3fdc9f0d3654c6a2e27
d.vorobjev <d.vorobjev@zordoc.uz>
2023-07-09 00:52:55 +0500

    implemented contacts page

---
+++ src/pages/contacts/contacts.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ContactList } from '~widgets/contact-list';
+import { MainLayout } from '~widgets/main-layout';
+</script>
+
+<template>
+  <MainLayout title="Список контактов">
+    <ContactList />
+  </MainLayout>
+</template>
